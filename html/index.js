284c5766ff962905a91a98674e6fa6894f37c276
Aalhad Ajay Kate <aalhadakate@gmail.com>
2022-03-12 12:40:09 +0530

    js

---
+++ html/index.js
@@ -0,0 +1,4 @@
+document.getElementById("bu").addEventListener("click", function() {
+    document.getElementById("body-1").style.display = "none";
+    document.getElementById("body-2").style.display = "block";
+})
