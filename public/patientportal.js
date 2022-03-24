05c6746a64e6df4ded3eac044ee7ce602016203a
Aalhad Ajay Kate <aalhadakate@gmail.com>
2022-03-24 12:19:23 +0530

    update

--- public/patientportal.js
+++ public/patientportal.js
@@ -6,6 +6,30 @@ $(document).ready(function(){
     });
   });
 });
+$(document).ready(function(){
+    $("#Input3").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable3 tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+$(document).ready(function(){
+    $("#Input2").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable2 tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+$(document).ready(function(){
+    $("#Input1").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable1 tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
 document.getElementById("strip").addEventListener("click", function() {
 
 
